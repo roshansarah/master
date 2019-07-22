7f0c1d2a5e0e1fbff413bfb0856a66074d700913
roshan <roshanonline8@gmail.com>
2019-07-22 07:02:41 -0400

    Using ngClass, classname, ngStyle

--- src/app/product-entry/product-entry.component.ts
+++ src/app/product-entry/product-entry.component.ts
@@ -12,8 +12,21 @@ export class ProductEntryComponent implements OnInit {
  productArray:Product[]=<any><Product[]>[ ]//inference type
  
   done=true
+  myColor='red'
+  myFont='bold'
+  myStyle={'color':this.myColor,'font-weight':this.myFont}
+  text="Welcome to Expertzlab!"
+
+  //For ngClass to work add the below property
+  blueBold='blueBold'
+
+  //using conditions
+  blueBoldDisabled='blueBoldDisabled'
+  disabled=false
+  
 
   ngOnInit() {
+    
   }
   
   saveProduct(pid,pname){
@@ -22,6 +35,10 @@ export class ProductEntryComponent implements OnInit {
   let product= new Product(pid,pname)
   console.log(product)
   this.productArray.push(product)
+ }
+
+ updateStyle(){
+  this.myStyle={'color':this.myColor,'font-weight':this.myFont}
  }
 
 
